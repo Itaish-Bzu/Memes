2847baa263988a611838e1c7740f32529ea2152b
Itaish-Bzu <Itaish94@gmail.com>
2025-07-19 14:06:01 +0300

    add filter

--- js/controller/galleryController.js
+++ js/controller/galleryController.js
@@ -1,37 +1,60 @@
 'use strict'
 
+let gFilter = ''
 
-
-function onShowGallery(){
-  document.querySelector('.gallery').style.display = 'block'
-  const elEditor = document.querySelector('.editor')
-  elEditor.style.display = 'none'
-   toggleMenu()
+function onShowGallery() {
+  toggleVisibility('editor')
+  toggleVisibility('gallery')
+  toggleMenu()
 }
 
-
 function renderGallery() {
-const imgs = gImgs
-  const strHTML = imgs.map(img =>{
-    return `<img src="${img.url}" onclick="onImgSelect(${img.id})" data-keys= ${img.keywords} >`})
+  const imgs = getImgs(gFilter) 
+
+  const strHTML = imgs.map((img) => {
+    return `<img src="${img.url}" onclick="onImgSelect(${img.id})" data-keys= ${img.keywords} >`
+  })
 
   document.querySelector('.gallery-photo').innerHTML = strHTML.join('')
 }
 
-function onImgSelect(imgID) {    
+function onImgSelect(imgID) {
   memeServiceSetImg(+imgID)
-  const elEditor = document.querySelector('.editor')
-  elEditor.style.display = 'block'
-  document.querySelector('.gallery').style.display = 'none'
-   renderMeme()
+
+  toggleVisibility('editor')
+
+  toggleVisibility('gallery')
+  renderMeme()
 }
 
-function onRandom(){
-    const elGallery =document.querySelector('.gallery')
-  if(elGallery.style.display === 'none') return
- const imgID = getRandomIntInclusive(1, gImgs.length)
- onImgSelect(imgID)
- toggleMenu()
+function onRandom() {
+  const elGallery = document.querySelector('.gallery')
+  if (elGallery.style.display === 'none') return
+  const imgID = getRandomIntInclusive(1, gImgs.length)
+  onImgSelect(imgID)
+  toggleMenu()
 }
 
+function onChoseFilter(val) {
+  gFilter = val
+  renderGallery()
+}
 
+// function onMemes(){
+// const imgs = saveMeme()
+//     const strHTML = imgs.map((img) => {
+//     return `<img src="${img.url}" onclick="onImgSelect(${img.id})" data-keys= ${img.keywords} >`
+//   })
+
+//   // document.querySelector('.gallery').style.display = 'none'
+//   // const elEditor = document.querySelector('.editor')
+//   // elEditor.style.display = 'none'
+// // const elSaved =  document.querySelector('.saved-imgs').style.display = 'block'
+// document.querySelector('.saved-imgs').innerHTML = strHTML.join('')
+// toggleVisibility('saved-imgs')
+
+// }
+
+function toggleVisibility(element) {
+  document.querySelector(`.${element}`).classList.toggle('hide')
+}
